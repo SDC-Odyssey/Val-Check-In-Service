4b031a0ff517d47e102d0a766dd76cd4d8f4d097
Peter Vu <pvu@stripe.com>
2020-08-11 21:18:35 -0700

    Installed esLint and corresponding AirBnB style guide. Refactored generateRandomData.js to match corresponding style

--- checkIn/server/generateRandomData.js
+++ checkIn/server/generateRandomData.js
@@ -1,28 +1,33 @@
-var createRandomCheckInData = function() {
-    var initArray = new Array(100);
-    var newRecords = initArray.map((currentValue, currentIndex) => {
-        var id = currentIndex;
-        var basePrice = Math.floor((Math.random() * 150)) + 40;
-        var cleaningFee = Math.floor((Math.random() * 30)) + 5;
-        var occupancyFee = Math.floor((Math.random() * 10)) + 5;
-        var costAdditionalPerson = Math.floor(Math.random() * 2) * 0.25 * basePrice;
+exports.createRandomCheckInData = function () {
+  const newRecords = [];
+  for (let i = 1; i < 101; i += 1) {
+    const id = i;
+    const basePrice = Math.floor((Math.random() * 150)) + 40;
+    const cleaningFee = Math.floor((Math.random() * 30)) + 5;
+    const occupancyFee = Math.floor((Math.random() * 10)) + 5;
+    const costAdditionalPerson = Math.floor(Math.random() * 2) * 0.25 * basePrice;
 
-        //the service fee is the base price * a random percentage between 3% and 15%.
-        var serviceFee = basePrice * (Math.floor(Math.random() * 10) + 3) /100;
+    // the service fee is the base price * a random percentage between 3% and 15%.
+    const serviceFee = (basePrice * (Math.floor(Math.random() * 10) + 3)) / 100;
 
-        //some random number between 0 and 15 nights.
-        var minimumNights = Math.floor(Math.random() * 14);
-        
-        return {
-            id: id,
-            base_price: basePrice,
-            cleaning_fee: cleaningFee,
-            occupancy_fee: occupancyFee,
-            cost_additional_person: costAdditionalPerson,
-            service_fee: serviceFee,
-            minimum_nights: minimumNights
-        }
+    // some random number between 0 and 15 nights.
+    const minimumNights = Math.floor(Math.random() * 14);
+
+    newRecords.push({
+      id,
+      base_price: basePrice,
+      cleaning_fee: cleaningFee,
+      occupancy_fee: occupancyFee,
+      cost_additional_person: costAdditionalPerson,
+      service_fee: serviceFee,
+      minimum_nights: minimumNights,
     });
+  }
 
-    return newRecords;
-}
+  return newRecords;
+};
+
+exports.createAvailabilityDates = function () {
+  const newDates = [];
+  
+};
