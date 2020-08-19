3f17d68e6b5eefccea409e8e91de59148f9d2faf
Peter Vu <pvu@stripe.com>
2020-08-19 09:09:21 -0700

    Added tests for random availability generator

--- checkIn/test/db.test.js
+++ checkIn/test/db.test.js
@@ -30,9 +30,21 @@ describe('Check-in database', () => {
 });
 
 describe('Random data generator', () => {
-  it('Generates a pricing array with an id', () => {
+  it('Generates a pricing array with an id, base price, and service fee', () => {
     const randomPricing = generatePricing();
     expect(Array.isArray(randomPricing)).toBe(true);
+    expect(randomPricing.length).toBeGreaterThan(0);
     expect(randomPricing[0].id).toBeTruthy();
+    expect(randomPricing[0].base_price).toBeTruthy();
+    expect(randomPricing[0].service_fee).toBeTruthy();
+  });
+  it('Generates an availability array with an id, room_id, date, and available boolean', () => {
+    const randomAvailability = generateAvailability();
+    expect(Array.isArray(randomAvailability)).toBe(true);
+    expect(randomAvailability.length).toBeGreaterThan(0);
+    expect(randomAvailability[0].id).toBeTruthy();
+    expect(randomAvailability[0].room_id).toBeTruthy();
+    expect(randomAvailability[0].date).toBeTruthy();
+    expect(randomAvailability[0].available).toBeTruthy();
   });
 });
