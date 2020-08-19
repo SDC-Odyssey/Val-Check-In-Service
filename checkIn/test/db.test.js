6bdef111e10edd65a18889456b1046268b08c4e0
Peter Vu <pvu@stripe.com>
2020-08-19 09:01:02 -0700

    Added tests for random pricing generator

--- checkIn/test/db.test.js
+++ checkIn/test/db.test.js
@@ -4,21 +4,35 @@ const {
   sequelize,
 } = require('../server/db');
 
+const {
+  generatePricing,
+  generateAvailability,
+} = require('../server/generateRandomData');
+
+// integration test for database
 describe('Check-in database', () => {
-  it('actually exists', async () => {
+  it('Actually exists', async () => {
     const databaseName = sequelize.getDatabaseName();
     expect(databaseName).toBe('check_in');
   });
 
-  it('has an availability table', async () => {
+  it('Has a pricing table', async () => {
     const pricingTableName = Pricing.getTableName();
     expect(pricingTableName).toBe('pricings');
   });
 
-  it('has an availability table', async () => {
+  it('Has an availability table', async () => {
     const availabilityTableName = Availability.getTableName();
     expect(availabilityTableName).toBe('availabilities');
   });
 
   sequelize.close();
+});
+
+describe('Random data generator', () => {
+  it('Generates a pricing array with an id', () => {
+    const randomPricing = generatePricing();
+    expect(Array.isArray(randomPricing)).toBe(true);
+    expect(randomPricing[0].id).toBeTruthy();
+  });
 });
