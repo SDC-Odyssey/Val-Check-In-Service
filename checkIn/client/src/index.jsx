42c8327a49f75e090d416ea3ed441105c10966be
Peter Vu <pvu@stripe.com>
2020-08-24 22:15:31 -0700

    Updated client side index.jsx to send a request to the proxy server. Also included the check-in bundle.js into the proxy. The next step is to build out proxy server endpoint, which should send separate requests to the check-in module and pass it back to the client. Afterwards, once that is running within the check-in module, the next step is to move the proxy server out of the check-in directory & into the broader project directory. Will likewise need to install required dependencies

--- checkIn/client/src/index.jsx
+++ checkIn/client/src/index.jsx
@@ -6,8 +6,13 @@ import axios from 'axios';
 import Test from './components/firstModule';
 
 const init = async function initializeApp() {
-  const pricingInformation = await axios.get('http://127.0.0.1:3000/pricing/1');
-  console.log(pricingInformation);
+  const url = new URL(window.location);
+  const endpoints = url.pathname.split('/');
+  // This should send a request to the proxy server
+  // The proxy server will then send a request to all other endpoints
+  const roomId = endpoints[1];
+  const pricingInformation = await axios.get(`http://127.0.0.1:3000/${roomId}`);
+  // console.log(pricingInformation);
   ReactDOM.render(<Test pricing={pricingInformation} />, document.getElementById('app'));
 };
 
