f51c57860ce9f20a616a2ad2aa3d65920e7e65f5
Valeriia Ten <tenv279@gmail.com>
2020-10-20 23:33:08 -0400

    complete seeder script for pg and script for creating table

--- checkIn/database/dataGeneration.js
+++ checkIn/database/dataGeneration.js
@@ -3,27 +3,25 @@ const fs = require('fs');
 
 const writeDataToTable = fs.createWriteStream('availabilities-table.csv', { flags: 'a' });
 
-const writeTenMillionUsers = (writer, encoding, callback) => {
+const generateRecords = (writer, enc, cb) => {
   let i = 10000000;
   let id = 0;
   const write = () => {
-    let ok = true;
+    let flag = true;
     do {
       i -= 1;
       id += 1;
-      const date = faker.date.soon();
-      const roomId = faker.random.number(1, 101);
+      const date = faker.date.soon().toISOString();
+      const roomId = faker.random.number({ min: 1, max: 101 });
       const available = faker.random.boolean();
       const data = `${id},${date},${roomId},${available}\n`;
       if (i === 0) {
-        writer.write(data, encoding, callback);
+        writer.write(data, enc, cb);
       } else {
-        ok = writer.write(data, encoding);
+        flag = writer.write(data, enc);
       }
-    } while (i > 0 && ok);
+    } while (i > 0 && flag);
     if (i > 0) {
-      // had to stop early!
-      // write some more once it drains
       writer.once('drain', write);
     }
   };
@@ -31,7 +29,8 @@ const writeTenMillionUsers = (writer, encoding, callback) => {
 };
 
 writeDataToTable.write('id,date,room_id,available\n', 'utf8');
-writeTenMillionUsers(writeDataToTable, 'utf-8', () => {
+
+generateRecords(writeDataToTable, 'utf-8', () => {
   writeDataToTable.end(err => {
     if (err) {
       console.log('Getting an error', err);
